9824a085864c93f8e5cfe21294a6b2b3766bae80
Endika <endikaorve@gmail.com>
2021-10-05 19:25:05 +0200

    Correcciones menores

--- src/components/PokeContainer/PokeContainer.jsx
+++ src/components/PokeContainer/PokeContainer.jsx
@@ -18,66 +18,21 @@ export const PokeContainer = (props) => {
     })
 
     React.useEffect(() => {
-
-        pokemonService.listDetailed().then(
+        dispatch({
+            type: 'IS_LOADING_POKEMONS'
+        })
+        pokemonService.listAll().then(
           pokemons => {
             dispatch({
               type: 'LOADING_POKEMONS_SUCCESS',
               data: pokemons
             })
-          }).catch(err => console.log('err',err));
-
-        //async function fetchPokemons() {
-        //    dispatch({
-        //        type: 'IS_LOADING_POKEMONS'
-        //    })
-////
-        //    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-        //    .then(
-        //        response => {
-        //            console.log('response', response)
-        //            const response_pokemons = response.data.results;
-        //            console.log('response po', response_pokemons)
-        //            Promise.all(
-        //                response_pokemons.map(rp => axios.get(rp.url).then(r => r.data))
-        //            )
-        //            .then(allPokemonData => {
-        //                dispatch({
-        //                    type: 'LOADING_POKEMONS_SUCCESS',
-        //                    data: allPokemonData.map(p => (
-        //                        {
-        //                            id: p['id'],
-        //                            name: p['name'],
-        //                            height: parseInt(p['height']) / 10,
-        //                            weight: parseInt(p['weight']) / 10,
-        //                            types: p['types'].map(type => type.type.name),
-        //                            img: p['sprites']['other']['official-artwork']['front_default'],
-        //                            alt_img: p['sprites']['other']['dream_world']['front_default'],
-        //                            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet facilisis mi. Integer eget urna eu mauris scelerisque ornare. Nam at finibus purus. Nam quis mauris elementum, ullamcorper sem non, convallis mi. Sed a bibendum sapien, non posuere ligula. Morbi sit amet maximus purus. Nulla eu pulvinar ante. Nulla gravida luctus convallis. Duis suscipit vel nisi et tincidunt. Aenean velit purus, rutrum euismod justo id, porta tincidunt orci. Curabitur quis nibh elit. Etiam ornare tortor ac nibh commodo dapibus. Proin vehicula erat sit amet lacus posuere tincidunt.'
-        //                        }
-        //                    ))
-        //                })
-        //            })
-        //            .catch(e => {
-        //                dispatch({
-        //                    type: 'IS_LOADING_POKEMONS',
-        //                    data: 'Error en el Promise.All'
-        //                })
-        //                console.error('error', e)
-        //            });
-        //        }
-        //    )
-        //    .catch(e => {
-        //        dispatch({
-        //            type: 'IS_LOADING_POKEMONS',
-        //            data: 'Error en el list'
-        //        })
-        //        console.error('error', e)
-        //    });
-//
-        //}
-//
-        //fetchPokemons()
+          }).catch(error => {
+            dispatch({
+                type: 'LOADING_POKEMONS_ERROR',
+                data: error
+            })
+          });
     }, [])
 
     return (
