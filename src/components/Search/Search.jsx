b154eb1ba3353cb346026d99430546f0b6cf12b0
Endika <endikaorve@gmail.com>
2021-10-15 21:12:22 +0200

    Selector de generaciones
    Co-authored-by: jartigag <jartigag@pm.me>

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,8 +2,40 @@ import search from './../../assets/img/icons/search.svg'
 
 import './Search.css';
 
+const pokemonGenerationOptions = [
+    {
+        label: 'Primera generación',
+        value: '1'
+    },
+    {
+        label: 'Segunda generación',
+        value: '2'
+    },
+    {
+        label: 'Tercera generación',
+        value: '3'
+    },
+    {
+        label: 'Cuarta generación',
+        value: '4'
+    },
+    {
+        label: 'Quinta generación',
+        value: '5'
+    }
+]
+
 export const Search = (props) => (
     <section>
+        <select onChange={e => props.onSelect(e)} value={props.generationValue}>
+            {
+                pokemonGenerationOptions.map(option => (
+                    <option key={option.label} value={option.value}>
+                        {option.label}
+                    </option>
+                ))
+            }
+        </select>
         <input onChange={e => props.onSearch(e)} style={{backgroundImage: `url(${search})`}}></input>
     </section>
 )
